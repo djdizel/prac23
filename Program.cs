f2c4d2d50ff1ca3a293446864618bfdef9b4ef2b
Kab-31-16 <Kab-31-16@KAB-31-16-PC>
2025-02-14 13:28:41 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //объект с именем rec,конструктор без параметров
+            Rectangle rec = new Rectangle();
+            Console.WriteLine($"{rec.ToString()}\n");
+            //конструктор с rec параметрами 
+            Rectangle rec2 = new Rectangle(4,3);
+            Console.WriteLine($"{rec2.ToString()}\n");
+            //конструктор с parse
+            Console.WriteLine("Введите длину");
+            double length = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите ширину");
+            double width = double.Parse(Console.ReadLine());
+            Rectangle rec3 = new Rectangle(length, width);
+            Console.WriteLine($"{rec3.ToString()}\n");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Rectangle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac23
+{
+    internal class Rectangle
+    {
+        /// <summary>
+        /// Объявление полей
+        /// </summary>
+        private double length;
+        private double width;
+        /// <summary>
+        /// Инкапсуляция полей
+        /// </summary>
+        public double Length { get => length; 
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Error");
+                length = value;
+            }
+        }
+        public double Width { get => width; 
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Error");
+                width = value;
+            }
+        }
+
+        /// <summary>
+        /// Конструктор без параметров
+        /// </summary>
+        public Rectangle() { }
+        /// <summary>
+        /// Конструктор с 1 параметром
+        /// </summary>
+        /// <param name="field1"></param>
+        public Rectangle(double length,double width)
+        {
+            this.Length = length;
+            this.Width = width;
+        }
+
+        /// <summary>
+        /// Функция формирования строки с информацией об объекте
+        /// </summary>
+        public override string ToString()
+        {
+            return $"Length = {Length}, Width = {Width}";
+        }
+    }
+}
